7c5db3017c61b670bb974a88f9c1b59b16ce124d
burbon-79 <ilya-ilya-2007@ukr.net>
2024-09-02 16:45:26 +0300

    Removed unnecessary interfaces

--- src/main/java/com/estore/controller/ErrorControllerImpl.java
+++ src/main/java/com/estore/controller/ErrorControllerImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorControllerImpl implements ErrorController {
+public class ErrorControllerImpl implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request){
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
